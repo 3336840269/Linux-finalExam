634f3460dd7d7bb64f2d2305b25d7171925c4430
3336840269 <154768790+3336840269@users.noreply.github.com>
2024-01-12 17:53:42 +0800

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -3,3 +3,7 @@ sudo vgcreate vg-a /dev/md5
 sudo vgs
 sudo lvcreate /dev/vg-a -L 9G -n lv-a
 sudo lvs
+sudo mkdir /folder-a
+sudo mkfs.ext4 /dev/vg-a/lv-a
+sudo bash -c 'echo "/dev/vg-a/lv-a  /folder-a       ext4    defaults        0 0" >> /etc/fstab'
+sudo mount -a
